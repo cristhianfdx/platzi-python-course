2e3e816efcf1847bf18bf8bca78cad86acb86441
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2020-01-01 18:46:28 -0500

    Add list-operations

---
+++ data-structure/lists-operations.py
@@ -0,0 +1,25 @@
+# Sumar elementos
+
+list_1 = [1]
+list_2 = [2, 3, 4]
+list_3 = list_1 + list_2  # [1, 2, 3, 4]
+
+# Multipicar elementos
+list_4 = ['a']
+list_5 = list_4 * 4  # ['a', 'a', 'a', 'a']
+
+# Añadir elementos -- Agrega un elemento al final
+list_1.append(23) # [1, 23]
+
+# Eliminar elementos -- elimina el último elemento
+list_1.pop(23) # [1]
+
+# Eliminar elemento deseado
+del list_2[0] # [3, 4]
+
+# Convertir string en lista
+list('Hola') # ['H', 'o', 'l', 'a']
+
+# Convertir lista en string
+''.join(['h', 'o', 'l', 'a']) # 'hola'
+
