42c88c52c1867f79f8701047ec61b1509ed2ba94
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2020-01-01 13:59:08 -0500

    Add data types

---
+++ data-types.py
@@ -0,0 +1,11 @@
+# Tipos de datos
+    # Integer <int>
+    # Float <float>
+    # String <str>
+    # Boolean <bool>
+
+# Obtener tipo
+print(type(2)) # <type 'int'>
+print(type(2.0)) # <type 'float'>
+print(type(True)) # <type 'bool'>
+print(type('Hello')) # <type 'str'>
