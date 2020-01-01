3f542f39d14f5acc486934f45de3c4ca73fd5f23
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2020-01-01 13:59:25 -0500

    Add operators types

---
+++ operators.py
@@ -0,0 +1,34 @@
+# operadores matematicos
+## suma
+print(3 + 5) # 5
+
+## resta
+print(3 - 5) # -2
+
+## multiplicacion
+print(5 * 2) # 10
+
+## division
+print(5 / 2) # 2
+print(5.0 / 2.0) # 2.5
+
+## modulo
+print(9 % 2) # 1
+
+# potencia
+print(3**2) # 9
+
+# operadores de comparacion
+print(3 > 5) # False
+print(3 < 5) # True
+print(10 == 10) # True
+print(10 == 9) # False
+print(10 >= 9) # True
+print(10 <= 9) # False
+
+# orden en las operaciones
+    # PEMDAS
+        # 1. Parentesis
+        # 2. Exponentes
+        # 3. Multiplicacion / Division
+        # 4. Adicion / sustraccion
