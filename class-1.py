d5e4c85b23574300631e026a7aa54113d14268c5
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2020-01-01 13:32:46 -0500

    Add first class

---
+++ class-1.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()
+cristhian = turtle.Turtle()
+cristhian.forward(50)
+cristhian.left(90)
+cristhian.forward(50)
+cristhian.left(90)
+cristhian.forward(50)
+cristhian.left(90)
+cristhian.forward(50)
+cristhian.left(90)
+
+turtle.mainloop()
