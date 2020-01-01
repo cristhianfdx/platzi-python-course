9f7c361a3e7d8596ba088a4113acea2bc2099089
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2020-01-01 17:57:19 -0500

    add while loop example

---
+++ loops/while_loop.py
@@ -0,0 +1,35 @@
+# i = 0
+
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+
+import random
+
+
+def run():
+    found = False
+    random_number = random.randint(0, 20)
+
+    while not found:
+        number = int(input('Intenta un número: '))
+
+        if number == random_number:
+            print('El número {} fue encontrado'.format(number))
+            found = True
+        elif number > random_number:
+            print('El número es mas pequeño')
+        else:
+            print('El número es mas grande')
+
+
+if __name__ == '__main__':
+    run()
+
