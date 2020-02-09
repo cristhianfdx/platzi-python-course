1c54ec45b8f9acaf4efae5283e382f6098db3c2c
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2020-02-09 16:00:02 -0500

    Add GUI practice

---
+++ gui/first_gui.py
@@ -0,0 +1,22 @@
+from tkinter import *
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def run():
+    main = Tk()
+    main.title('Test window')
+
+
+    if ( sys.platform.startswith('win')):
+        main.iconbitmap(os.path.join(BASE_DIR, 'favicon.ico'))
+    else:
+        logo = PhotoImage(file=os.path.join(BASE_DIR, 'favicon.png'))
+        main.tk.call('wm', 'iconphoto', main._w, logo)
+
+    main.geometry('650x350')
+    main.config(bg='blue')
+    main.mainloop()
+
+if __name__ == '__main__':
+    run()
